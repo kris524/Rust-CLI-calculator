42066de852a16130170c225e25c7cce6b52dc97a
Kris <kristian_dilov@abv.bg>
2022-05-11 23:20:15 +0100

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,21 @@ struct Calc {
 
 }
 
+
 fn main() {
     let args = Calc::parse();
+    // let commands = Calc {
+    //     first_number: std::env::args().nth(1).expect("no number is given"),
+    //     operator: std::env::args().nth(2).expect("no operation selected"),
+
+
+
+    // }
+    // let Calc::first_number = 
+
+
     // let first_number = std::env::args().nth(1).expect("no number is given");
-    // let operator = std::env::args().nth(2).expect("no operation selected");
-    // let second_number = std::env::args().nth(3).expect("no number is given");
+    let operator: String = std::env::args().nth(2).expect("no operation selected");
+    let second_number: u32 = std::env::args().nth(3).expect("no number is given");
 
 }

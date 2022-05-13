a7680e704f29a5a70c67d67dcd0a086e78386553
Kris <kristian_dilov@abv.bg>
2022-05-13 23:44:46 +0100

    Small changes/Simplify

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,44 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    first_number: f32,
+    first_number: String,
     operation: char,
-    second_number: f32,
+    second_number: String,
 
 }
 
-fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+impl Cli {
 
-    let result = match operator {
-        '+' => first_number + second_number,
-        '-' => first_number - second_number,
-        '*' => first_number * second_number,
-        '/' => first_number / second_number,
-        _ => panic!("Invalid operation")
+    fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
 
-    };
-    return result
+        let result = match operator {
+            '+' => first_number + second_number,
+            '-' => first_number - second_number,
+            '*' => first_number * second_number,
+            '/' => first_number / second_number,
+            _ => panic!("Invalid operation")
+
+        };
+        return result
+}
 }
 
 fn main() {
 
-    // let args = Cli::parse();
-    
-    
-    let first: String = std::env::args().nth(1).unwrap(); 
-    println!("The first number is: {}", first);
-    let operator: char = std::env::args().nth(2).unwrap().chars().next().unwrap();
-    println!("The operator is: {}", operator);
-    let second: String = std::env::args().nth(3).unwrap();
-    println!("The second number is: {}", second);
-
-    let first_number = first.parse::<f32>().unwrap();
-    let second_number = second.parse::<f32>().unwrap();
+    let commands = Cli {
+        first_number: std::env::args().nth(1).unwrap(),
+        operation: std::env::args().nth(2).unwrap().chars().next().unwrap(),
+        second_number: std::env::args().nth(3).unwrap()
+    };
     
 
-    let result = operate(operator, first_number, second_number );
+    let first_number = commands.first_number.parse::<f32>().unwrap();
+    let second_number = commands.second_number.parse::<f32>().unwrap();
+    
+
+    let result = operate(commands.operation, first_number, second_number );
     println!("Result: {}", result);
 
-    let m = Cli {
-        first_number: 1.0,
-        operation: '+',
-        second_number: 4.0
-    };
+    
 }
  
